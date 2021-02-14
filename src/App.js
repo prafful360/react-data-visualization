a035ff28e9a0cd85805c8f956d6549fdba111ee1
prafful360 <praffulranjan54@gmail.com>
2021-02-15 04:29:40 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,81 +1,76 @@
 import "./App.css";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, Line, Radar } from "react-chartjs-2";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import axiosRetry from "axios-retry";
 
 function App() {
-  // axiosRetry(axios, { retries: 1 });
-  const [employees, setEmployees] = useState([]);
-  // const [age, setAge] = useState([]);
-  // const [salary, setSalary] = useState([]);
-  const data = {
-    labels: ["12", "19", "3", "5"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5],
+  const [chartData, setChartData] = useState({});
+  const [employeeSalary, setEmployeeSalary] = useState([]);
+  const [employeeAge, setEmployeeAge] = useState([]);
 
-        backgroundColor: [
-          "rgba(33,43,64,1)", //baker
-          "rgba(194,244,120,1)", //benton
-          "rgba(223,211,182,1)", //clackamas
-          "rgba(186,220,221,1)",
-        ],
-        // borderColor: "rgba(255, 99, 132, 0.2)",
-      },
-    ],
+  const chart = () => {
+    let data1 = [];
+    let data2 = [];
+    const url1 = "https://pokeapi.co/api/v2/pokemon/1";
+    const urlEmp = "http://dummy.restapiexample.com/api/v1/employees";
+    axios
+      .get(url1)
+      .then((res) => {
+        console.log(res);
+
+        setChartData({
+          labels: data1,
+          datasets: [
+            {
+              label: "Ratio B/W Age and Salary",
+              data: data2,
+              backgroundColor: [
+                "rgba(156,39,176 ,0.5)",
+                // "rgba(205,220,57 ,1)",
+                // "rgba(2,136,209 ,1)",
+                "rgba(156,39,176 ,1)",
+              ],
+              // borderWidth: 4,
+            },
+          ],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(data1, data2);
   };
-  //   async function getUsers() {
-  //     let url = 'http://dummy.restapiexample.com/api/v1/employees?_limit=20';
-  //     try {
-  //         let res = await fetch(url);
-  //         return await res.json();
-  //     } catch (error) {
-  //         console.log(error);
-  //     }
-  // }
-  useEffect(async () => {
-    let empSal = [];
-    let empAge = [];
-    const url = "http://dummy.restapiexample.com/api/v1/employees";
-    await axios.get(url, { timeout: 11 }).then((res) => {
-      // const interval = 160;
-      // let promise = Promise.resolve();
-      console.log(res);
-      for (const dataObj of res.data.data) {
-        empSal.push(parseInt(dataObj.employee_salary));
-        console.log(dataObj.employee_salary);
-      }
-      // console.log(employees);
-      // return new Promise((resolve) => {
-      //   setTimeout(resolve, interval);
-      // });
-    });
-  }, []);
 
+  useEffect(() => {
+    chart();
+  }, []);
   return (
     <div className="App">
-      <div className="container center-flex">
-        <div
+      <div className=" center-flex">
+        {/* <div
           className="center-flex"
           style={{
-            width: "35vh",
-            height: "20vh",
+            width: "50vh",
+            height: "50vh",
             backgroundColor: "#E0E0E0",
             display: "inline-block",
             marginTop: "1rem",
             borderRadius: "6px",
           }}
         >
-          <Doughnut
-            data={data}
+
+
+        </div> */}
+        <div className="contain-data-el">Data Visualization Using Doughnut</div>
+        <div className="container">
+          <Radar
+            data={chartData}
             options={{
               responsive: true,
             }}
           />
         </div>
-        <div className="contain-data-el">Data Visualization Using Doughnut</div>
       </div>
     </div>
   );
